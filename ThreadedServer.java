88be194c33efbe2ddea7bfb93bca38f8423040cf
konra <konra@LAPTOP-8BFLOGCI>
2017-12-09 20:59:44 +0100

    First comit

---
+++ ThreadedServer.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+public class ThreadedServer {
+
+	public static void main(String[] args) {
+
+		try {
+			
+			ServerSocket server = new ServerSocket(8189);
+			int i = 1;
+			
+			while(true) {
+				Socket coming = server.accept();
+				
+				System.out.println("Po³¹czono" + i);
+				
+				Runnable r = new ThreadedServerHandler(coming, "name " + i);
+				Thread t = new Thread(r);
+				t.start();
+				i++;
+				
+				
+				
+			}
+			
+		}
+		catch(IOException e) {			
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ ThreadedServerHandler.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ThreadedServerHandler implements Runnable {
+	
+	private Socket coming;
+	private String name;
+
+	public ThreadedServerHandler(Socket coming, String name) {
+		
+		this.coming = coming;
+		this.name = name;
+	}
+	
+	
+	@Override
+	public void run() {
+		
+		
+		try {
+			InputStream inStream = coming.getInputStream();
+			OutputStream outStream = coming.getOutputStream();
+			
+			Scanner in = new Scanner(inStream);
+			PrintWriter out = new PrintWriter(outStream, true);
+			
+			out.println("Witaj " + name + " co u ciebie ?");
+
+			while(in.hasNextLine()) {
+				String line = in.nextLine();
+				out.println(name + "powiedzia³: " + line);
+				
+			}
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+
+}
