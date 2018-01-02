9d26e3ca78f1b1a2ce8bc015f24f2f2bb2235f26
kapi10072 <kapi10072@gmail.com>
2018-01-02 17:14:56 +0100

    MessageTest updated

--- Tests/MessageTest.java
+++ Tests/MessageTest.java
@@ -1,14 +1,19 @@
 package Tests;
 
 import Client.Client;
-import Message.SuccessMessage;
+import Game.ColorPlayer;
+import Message.*;
 import Server.ThreadedServer;
+import javafx.application.Platform;
 import org.junit.*;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 /**
- * Created by Kacper on 2018-01-01.
+ * This class is testing message's methods
  */
 public class MessageTest
 {
@@ -26,20 +31,125 @@ public class MessageTest
 			serverThread = new Thread(r);
 			serverThread.start();
 		}
+
+		Thread thread = new Thread(() ->
+		{
+			try
+			{
+				Platform.runLater(() ->
+				{
+					myClient = new Client();
+					try {
+						myClient.connectServer();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+				Thread.sleep(1000);
+				Platform.runLater(() ->
+				{
+
+				});
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		});
+		thread.start();
 	}
 
 	@Test
-	public void getInstance() throws Exception
+	public void sendSuccessMessage() throws Exception
 	{
 		SuccessMessage newMessage = new SuccessMessage(22222, "remove game");
+		doLatency(newMessage);
+		if(newMessage.getCodSuccess() == 22222 && newMessage.getDescription().equals("remove game"))
+			assertTrue(true);
+	}
 
-		try
+	@Test
+	public void sendNewNameMessage() throws Exception
+	{
+		NewNameMessage newMessage = new NewNameMessage("Tested name");
+		doLatency(newMessage);
+
+		if(newMessage.getName().equals("Tested name"))
+			assertTrue(true);
+	}
+
+	@Test
+	public void sendMoveMessage() throws Exception
+	{
+		MoveMessage newMessage = new MoveMessage(true);
+		doLatency(newMessage);
+
+		if(newMessage.getEndTurn())
+			assertTrue(true);
+	}
+
+	@Test
+	public void sendJoinGameMessage() throws Exception
+	{
+		JoinGameMessage newJoinMessage = new JoinGameMessage(0, true);
+		doLatency(newJoinMessage);
+		if(newJoinMessage.getIdGame() == 0 && newJoinMessage.getNewGame())
+			assertTrue(true);
+	}
+
+	@Test
+	public void sendFailMessage() throws Exception
+	{
+		FailMessage failMessage = new FailMessage(1234, "Test failure message");
+		doLatency(failMessage);
+		if(failMessage.getCodFail() == 1234 && failMessage.getDescription().equals("Test failure message"))
+			assertTrue(true);
+	}
+
+	@Test
+	public void sendChatMessage() throws Exception
+	{
+		ChatMessage newMessage = new ChatMessage("Hello world");
+		doLatency(newMessage);
+		if(newMessage.getDescription().equals("Hello world"))
+			assertTrue(true);
+	}
+
+	@Test
+	public void sendAddPawnMessage() throws Exception
+	{
+		AddPawnMessage newMessage = new AddPawnMessage(ColorPlayer.PLAYER_FIVE, 3, 4);
+		doLatency(newMessage);
+		if (newMessage.getMyX() == 3 && newMessage.getMyY() == 4 &&
+				newMessage.getPlayer().equals(ColorPlayer.PLAYER_FIVE))
 		{
-			myClient.sendMessage(newMessage);
+			assertTrue(true);
 		}
-		catch (IOException e) {
+	}
 
-		}
+	@Ignore
+	private void doLatency(Message newMessage)
+	{
+		Thread thread = new Thread(() ->
+		{
+			try
+			{
+				Thread.sleep(1000);
+				Platform.runLater(() ->
+				{
+					try {
+						myClient.sendMessage(newMessage);
+					} catch (IOException e) {
+						fail();
+					}
+				});
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		});
+		thread.start();
 	}
 
 }
